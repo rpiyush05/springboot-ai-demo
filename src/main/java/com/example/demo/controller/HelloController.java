036c5d047fa426e7d37f499cfd87173ffaa0d188
rpiyush05 <122092721+rpiyush05@users.noreply.github.com>
2025-11-07 14:46:47 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World! 👋 Spring Boot is up and running!";
+        return "Hello, World! 👋 Spring Boot is up";
     }
 }
